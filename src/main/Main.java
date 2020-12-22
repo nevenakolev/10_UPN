a72f2ef3fef76f33a0b9b7a4ccf5276feb74fbbc
Nevena Kolev <nevena.kolev@htl-wels.at>
2020-12-22 21:26:01 +0100

    initial commit, VM-options are set

---
+++ src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Nevena Kolev
+ * @version 1.0, 17.12.2020
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
